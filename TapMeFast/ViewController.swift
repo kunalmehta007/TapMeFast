085ed941cc82e76c73d10b0637d6bc8a8169d344
kunalmehta <kunalmehta@kunals-macbook-pro-2.local>
2018-07-15 07:41:28 +0530

    Initial Commit

---
+++ TapMeFast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapMeFast
+//
+//  Created by Kunal Mehta on 15/07/18.
+//  Copyright © 2018 Kunal Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
