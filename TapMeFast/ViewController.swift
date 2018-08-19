e6d21f1929c91770ae7a3d2c24dd1c15f2b0c249
kunalmehta <kunalmehta@Kunals-MacBook-Pro-2.local>
2018-08-19 18:13:38 +0530

    TAP ME FAST
    A very basic and simple game where after the game starts the App
    records how many times have you tapped the button.
    This is the first app that I created on Xcode using NS Timers.

--- TapMeFast/ViewController.swift
+++ TapMeFast/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerlabel: UILabel!
+    @IBOutlet weak var scorelabel: UILabel!
+    @IBOutlet weak var TapMeButton: UIButton!
+    @IBOutlet weak var startgamebutton: UIButton!
+    
+    var timer = Timer()
+    
+    var timeInt = 0
+    var TapInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeInt = 10
+        TapInt = 0
+        
+        timerlabel.text = String(timeInt)
+        scorelabel.text = String(TapInt)
+        
+        TapMeButton.isEnabled = false
+        TapMeButton.alpha = 0.5
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func TapMe(_ sender: Any) {
+        
+        TapInt += 1
+        scorelabel.text = String(TapInt)
+    
+    }
+    
+    @IBAction func StartGame(_ sender: Any) {
+        
+        if timeInt == 10 {
+            
+            TapInt = 0
+            scorelabel.text = String(TapInt)
+            
+            TapMeButton.isEnabled = true
+            TapMeButton.alpha = 1
+            
+            startgamebutton.isEnabled = false
+            startgamebutton.alpha = 0.5
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.startcounter), userInfo: nil, repeats: true)
+            
+        }
+        
+        if timeInt == 0 {
+            timeInt = 10
+            TapInt = 0
+            
+            timerlabel.text = String(timeInt)
+            scorelabel.text = String(TapInt)
+            
+            startgamebutton.setTitle("START GAME", for: [])
+            
+        }
+        
+    }
+    
+    @objc func startcounter() {
+        timeInt -= 1
+        timerlabel.text = String(timeInt)
+        
+        if timeInt == 0 {
+            timer.invalidate()
+            TapMeButton.isEnabled = false
+            TapMeButton.alpha = 0.5
+            
+            startgamebutton.isEnabled = true
+            startgamebutton.alpha = 1
+            
+            startgamebutton.setTitle("RESTART", for: [])
+        }
+    }
+    
+    
+    
+    
 }
 
